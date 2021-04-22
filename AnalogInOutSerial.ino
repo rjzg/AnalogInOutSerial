ac367d6d364c9ba36c5b600ce26ed34b5f11591c
rodriguinho <81981576+rjzg@users.noreply.github.com>
2021-04-22 13:46:40 -0300

    Add files via upload

--- AnalogInOutSerial.ino
+++ AnalogInOutSerial.ino
@@ -1,30 +1,29 @@
+const int analogInPin = A0;  
+const int analogOutPin = 9; 
+int sensorValue = 0;        
+int outputValue = 0;        
+
 void setup() {
+
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(A0, INPUT);
-  pinMode(7, OUTPUT);
-  }
+  
+}
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  int f = map(sensorValue,0,1023,10000,1000);
-  
-  Serial.print("sensor = ");
-  Serial.print(sensorValue);
-  Serial.println();
-    
-  Serial.print("frequency = ");
-  Serial.print(f);
-  Serial.println();
 
-  
-  if(sensorValue> 1000){
-    digitalWrite(7 ,HIGH);
-    noTone(3);
-      }else {
-        digitalWrite(7, LOW); 
-        tone(3,f);
-      
-   delay(2);
-  }
+  sensorValue = analogRead(analogInPin);
+
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+
+  analogWrite(analogOutPin, outputValue);
+
+  Serial.print("sensor = ");
+
+  Serial.print(sensorValue);
+
+  Serial.print("\t output = ");
+
+  Serial.println(outputValue);
+
+  delay(2);
 }
